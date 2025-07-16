1c0ea7d085f2a80bf3733f1e95b74d6b1fe0dde0
oluwatobi <uphold.aladejaretobi@gmail.com>
2025-07-16 11:23:38 +0100

    Convert movie-app-frontend from submodule to normal folder

---
+++ movie-app-frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Login from './pages/login';
+
+function App() {
+  return (
+    <div>
+      <h1>Movie App</h1>
+      <Login />
+    </div>
+  );
+}
+
+export default App;
